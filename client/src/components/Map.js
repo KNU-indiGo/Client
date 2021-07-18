89c9f35ba10ae2a7d9964a8c458b3362f8db79e3
Go-Jaecheol <gojaech@naver.com>
2021-07-18 16:04:49 +0900

    Add InfoWindow

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -6,10 +6,11 @@ const Map = () => {
     const [lng, setLng] = useState(128.61360539999998);
 
     const locations = [
-        {lat: 35.8856424, lng: 128.61360539999998},
-        {lat: 35.885442, lng: 128.615533},
+        {place: "경북대", lat: 35.8856424, lng: 128.61360539999998},
+        {place: "떠콜", lat: 35.885442, lng: 128.615533},
         
     ]
+    
 
     const mapStyles = {
         width: '70%',
@@ -20,15 +21,34 @@ const Map = () => {
         console.log(position.coords.longitude);
     }
     const renderMarkers = (map, maps) => {
+        var infowindow = new maps.InfoWindow();
         const markers = locations.map((location, i) => {
-            return new maps.Marker({
-              position: location,
-              map,
-            });
+            var marker = new maps.Marker({
+                position: location,
+                map,
+              });
+            maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    //html로 표시될 인포 윈도우의 내용
+                    infowindow.setContent(locations[i].place);
+                    //인포윈도우가 표시될 위치
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+            
+            if (marker) {
+                marker.addListener('click', function() {
+                    //중심 위치를 클릭된 마커의 위치로 변경
+                    map.setCenter(this.getPosition());
+                    //마커 클릭 시의 줌 변화
+                    map.setZoom(14);
+                });
+            }
+            return marker;
         });
         
     };
-
+    
     return (
         <div className="map">
             <GoogleMap 
