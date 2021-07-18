fec583d6229468e0c524224048768346b571ddaa
Go-Jaecheol <gojaech@naver.com>
2021-07-18 14:19:31 +0900

    Change Map api

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,31 +1,37 @@
-import kind from '@enact/core/kind';
-import PropTypes from 'prop-types';
+import React, {useEffect, useState} from "react";
 import GoogleMap from "google-map-react";
 
-const Map = kind ({
-    name: 'Map',
-
-    propTypes: {
-        lat: PropTypes.number,
-        lng: PropTypes.number
-    },
-
-    styles: { 
-        className: 'map'
-    },
-
-    render: ({lat, lng, ...rest}) => {
-        return (
-            <div className="map">
-                <GoogleMap 
-                    bootstrapURLKeys = {{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
-                    style={{width: '100%', height: '100%'}}
-                    defaultZoom={15}
-                    defaultCenter={{ lat: 37.5, lng: 127 }}
-                ></GoogleMap>
-            </div>
-        );
+const Map = () => {
+    const [lat, setLat] = useState(35.8856424);
+    const [lng, setLng] = useState(128.61360539999998);
+    const mapStyles = {
+        width: '70%',
+        height: '70%',
+    };
+    const showPosition = (position) => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
     }
-});
+    const renderMarkers = (map, maps) => {
+        let marker = new maps.Marker({
+        position: { lat: lat, lng: lng },
+        map,
+        title: 'Hello World!'
+        });
+        return marker;
+       };
+    return (
+        <div className="map">
+            <GoogleMap 
+                bootstrapURLKeys = {{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+                style={mapStyles}
+                defaultZoom={15}
+                defaultCenter={{ lat: lat, lng: lng }}
+                onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
+            >
+            </GoogleMap>
+        </div>
+    );
+};
 
 export default Map;
