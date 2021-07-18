597d1d9c7a09bcf9301c829c2cd095305fd8d90c
Go-Jaecheol <gojaech@naver.com>
2021-07-18 15:50:40 +0900

    Add Map marker

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -4,6 +4,13 @@ import GoogleMap from "google-map-react";
 const Map = () => {
     const [lat, setLat] = useState(35.8856424);
     const [lng, setLng] = useState(128.61360539999998);
+
+    const locations = [
+        {lat: 35.8856424, lng: 128.61360539999998},
+        {lat: 35.885442, lng: 128.615533},
+        
+    ]
+
     const mapStyles = {
         width: '70%',
         height: '70%',
@@ -13,17 +20,19 @@ const Map = () => {
         console.log(position.coords.longitude);
     }
     const renderMarkers = (map, maps) => {
-        let marker = new maps.Marker({
-        position: { lat: lat, lng: lng },
-        map,
-        title: 'Hello World!'
+        const markers = locations.map((location, i) => {
+            return new maps.Marker({
+              position: location,
+              map,
+            });
         });
-        return marker;
-       };
+        
+    };
+
     return (
         <div className="map">
             <GoogleMap 
-                bootstrapURLKeys = {{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+                bootstrapURLKeys = {{ key: process.env.REACT_APP_GOOGLE_API_KEY, libraries: 'places' }}
                 style={mapStyles}
                 defaultZoom={15}
                 defaultCenter={{ lat: lat, lng: lng }}
