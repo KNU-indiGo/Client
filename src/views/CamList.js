b76451c492c1bcb1838d8eede4d4299405da00a4
Juyoung Kim <nullyng@gmail.com>
2021-08-01 23:47:59 +0900

    Change from class component to function component

--- src/views/CamList.js
+++ src/views/CamList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Panel } from '@enact/sandstone/Panels';
 import Scroller from '@enact/sandstone/Scroller';
 import ThemeDecorator from '@enact/sandstone/ThemeDecorator';
@@ -12,48 +12,26 @@ import StatisticsButton from '../components/button/StatisticsButton';
 import GoBackButton from '../components/button/GoBackButton';
 import BottomNav from '../components/nav/BottomNav';
 
-const cams = [
-  {id: 1, name: "cam1"},
-  {id: 2, name: "cam2"},
-  {id: 3, name: "cam3"}
-]
+const CamList = (props) => {
+    const [building, setBuilding] = useState([])
 
-class CamList extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            place: []
-        };
-    }
-    componentDidMount() {
+    useEffect(() => {
         axios({
             url: "http://ec2-52-78-90-230.ap-northeast-2.compute.amazonaws.com:8080/api/fire/list",
             method: 'GET'
-        }).then((res) => {
+            }).then((res) => {
             res.data.map((place) => {
-              if (place.building_name === this.props.location.state.name)
-                this.setState({place : place});
-            });
+            if (place.building_name === props.location.state.name)
+              setBuilding(place);
         });
-    }
-    componentDidUpdate() {
-        axios({
-            url: "http://ec2-52-78-90-230.ap-northeast-2.compute.amazonaws.com:8080/api/fire/list",
-            method: 'GET'
-        }).then((res) => {
-            res.data.map((place) => {
-              if (place.building_name === this.props.location.state.name)
-                this.setState({place : place});
-            });
-        });
-    }
+      });
+    })
 
-    render() {
-      return(
+    return(
         <Panel style={{background: 'white', color: 'black'}}>
             <TopNav 
-            title={this.props.location.state.name} 
-            subtitle={this.props.location.state.addr} />
+            title={props.location.state.name} 
+            subtitle={props.location.state.addr} />
             <Scroller>
               <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", textAlign: "center" }}>
                 {cams.map((cam, key) => {
@@ -62,14 +40,19 @@ class CamList extends React.Component{
               </div>
               <GoBackButton />
               <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                <StatisticsButton id={this.state.place.id}/>
-                  {(this.state.place.status === "BREAKOUT") ? <OngoingButton/> : ((this.state.place.status === "CONTAIN") ? <CompleteButton/> : "")}
+                <StatisticsButton id={building.id} />
+                  {(building.status === "BREAKOUT") ? <OngoingButton/> : ((building.status === "CONTAIN") ? <CompleteButton/> : "")}
               </div>
               <BottomNav/>
               </Scroller>
         </Panel>
     )
 }
-}
+
+const cams = [
+  {id: 1, name: "cam1"},
+  {id: 2, name: "cam2"},
+  {id: 3, name: "cam3"}
+]
 
 export default ThemeDecorator(CamList);
