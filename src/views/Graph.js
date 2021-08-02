831a07f6c0182e760911caadbd9ee91f33c9cd70
Juyoung Kim <nullyng@gmail.com>
2021-08-03 01:18:59 +0900

    Update Graph css

--- src/views/Graph.js
+++ src/views/Graph.js
@@ -1,6 +1,9 @@
 import { Panel } from '@enact/sandstone/Panels';
 import ThemeDecorator from '@enact/sandstone/ThemeDecorator';
+import { Cell, Row } from '@enact/ui/Layout';
+import Scroller from '@enact/ui/Scroller';
 import React from 'react';
+import BottomNav from '../components/nav/BottomNav';
 
 import TopNav from '../components/nav/TopNav';
 
@@ -10,17 +13,28 @@ const Graph = (props) => {
         <Panel style={{background: 'white', color: 'black'}}>
             <TopNav
             title="Statistics"
+            subtitle={props.location.state.name}
             back_history={props.location.state.back_history}
             id={props.location.state.id}/>
-            <div> {props.location.state.name} </div>
-            <img
-              src={props.location.state.image_url[0]}
-              alt="live_stream"
-              style={{ width: "600px", height: "600px" }}/>
-              <img
-              src={props.location.state.image_url[1]}
-              alt="live_stream"
-              style={{ width: "600px", height: "600px" }}/>
+            <Scroller style={{ textAlign: 'center' }}>
+              <Row style={{ textAlign: 'center' }}>
+                <Cell>
+                  <h4> Shortest path to the location </h4>
+                  <img
+                  src={props.location.state.image_url[0]}
+                  alt="live_stream"
+                  style={{ width: "600px", height: "600px" }}/>
+                  </Cell>
+                <Cell>
+                  <h4> asdf </h4>
+                  <img
+                  src={props.location.state.image_url[1]}
+                  alt="live_stream"
+                  style={{ width: "600px", height: "600px" }}/>
+                </Cell>
+              </Row>
+              <BottomNav />
+            </Scroller>
         </Panel>
       )
 }
